6209130b7978a55149e020ce4ba6b54e3b243e90
Philip Lane <162570431+ple34@users.noreply.github.com>
2025-08-08 11:21:08 +0100

    Upload PaperForge script and Source directory

---
+++ Handle-PFFiles.ps1
@@ -0,0 +1,51 @@
+# Get the current working directory
+$currentDir = Get-Location
+
+# Define the source directory containing the zip files
+$sourceDir = Join-Path $currentDir "Source"
+
+# Ensure the source directory exists
+if (-Not (Test-Path $sourceDir)) {
+    Write-Error "Source directory does not exist: $sourceDir"
+    exit
+}
+
+# Get all zip files in the source directory matching the pattern
+$zipFiles = Get-ChildItem -Path $sourceDir -Filter "*.zip" #| Where-Object { $_.Name -match $zipPattern }
+
+# Process each matching zip file
+Write-Output "Extracting Zip Files..."
+foreach ($zipFile in $zipFiles) {
+    # Extract the zip file to the current working directory
+    $sourcePath = $zipFile.FullName
+    Expand-Archive -LiteralPath $sourcePath -DestinationPath $currentDir -Force
+}
+
+# Rename the resulting directories
+$rawMiniDirectories = Get-ChildItem -Path $currentDir -Directory | Where-Object { $_.Name -match "Paperforge.+Tier\d" }
+
+Write-Output "Renaming Directories..."
+foreach ($miniDir in $rawMiniDirectories) {
+    $newMiniDir = $miniDir.Name -replace "\[.+\]", ""
+    $newMiniDir = $newMiniDir -replace "_Tier\d", ""
+    $newMiniDir = $newMiniDir -replace "Paperforge", ""
+    $newMiniDir = $newMiniDir -replace "_", " - "
+    $pfId = $newMiniDir.Split(" ")[0]
+    Rename-Item -LiteralPath $miniDir.Name -NewName $newMiniDir -Force
+
+    # CutFile
+    $cutDir = Get-ChildItem | Where-Object { $_.Name -match "$pfId" } | Where-Object { $_.Name -match "Cutfile" }
+    $newCutDir = $cutDir.Name -replace "\[.+\]", ""
+    $newCutDir = $newCutDir -replace "Paperforge", ""
+    $newCutDir = $newCutDir -replace "_.+", ""
+    $pfId = $newCutDir
+    $newCutDir = $newCutDir + " - Cutfile"
+    Move-Item -LiteralPath $cutDir -Destination $(Join-Path $newMiniDir $newCutDir) -Force
+
+    # VTT File
+    $vttDir = Get-ChildItem | Where-Object { $_.Name -match "^VTT$pfId" }
+    $newVttDir = $vttDir.Name.Split("_")[0]
+    Move-Item -LiteralPath $vttDir -Destination $(Join-Path $newMiniDir $newVttDir) -Force
+}
+
+Write-Host "Processing complete."
